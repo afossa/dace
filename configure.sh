73f572434f66d2b430bd44223f4ec05912531514
Alberto Fossà <alberto.fossa18@gmail.com>
2024-04-17 12:12:32 -0500

    expose abs() in Julia

--- configure.sh
+++ configure.sh
@@ -8,6 +8,7 @@ mkdir build && cd build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=/workspace/programs/julia/dev/DACE_jll/override \
     -DCMAKE_PREFIX_PATH=/workspace/programs/julia/artifacts/5209ca23f516fb3391b885eef717e49b4ee0a268 \
+    -DCMAKE_INSTALL_RPATH=/workspace/programs/julia/artifacts/5209ca23f516fb3391b885eef717e49b4ee0a268/lib:/workspace/programs/julia/juliaup/julia-1.10.2+0.x64.linux.gnu/lib \
     -DCMAKE_BUILD_TYPE=Release \
     -DWITH_PTHREAD=ON \
     -DWITH_ALGEBRAICMATRIX=ON \

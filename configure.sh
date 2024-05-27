2cc2c68740eb36dd81be3edd8c366b627c7067d2
Alberto Fossà <alberto.fossa18@gmail.com>
2024-05-27 08:58:43 -0500

    added option to build dace without Julia interface

--- configure.sh
+++ configure.sh
@@ -4,18 +4,34 @@
 rm -rf build
 mkdir build && cd build
 
-# get the installation and prefix paths
+# get the Julia root path
 JULIA_ROOT_PATH=$1
-CXX_WRAP_PREFIX_PATH="/artifacts/c129d84767ca7fe64514b3789c623e1203355949"
 
 # configure the project
-cmake .. \
-    -DCMAKE_INSTALL_PREFIX="${JULIA_ROOT_PATH}/dev/DACE_jll/override" \
-    -DCMAKE_PREFIX_PATH="${JULIA_ROOT_PATH}${CXX_WRAP_PREFIX_PATH}" \
-    -DCMAKE_INSTALL_RPATH="${JULIA_ROOT_PATH}${CXX_WRAP_PREFIX_PATH}/lib:${JULIA_ROOT_PATH}/juliaup/julia-1.10.2+0.x64.linux.gnu/lib" \
+if [ -z "$1" ]; then
+    echo "Building dace without Julia interface"
+
+    cmake .. \
+    -DCMAKE_INSTALL_PREFIX="${HOME}/.local" \
     -DCMAKE_BUILD_TYPE=Release \
     -DWITH_PTHREAD=ON \
     -DWITH_ALGEBRAICMATRIX=ON \
-    -DCMAKE_CXX_STANDARD=17 \
-    -DWITH_JULIA=ON \
+    -DCMAKE_CXX_STANDARD=20 \
+    -DWITH_JULIA=OFF \
     -LA
+else
+    echo "Building dace with Julia interface"
+
+    CXX_WRAP_PREFIX_PATH="/artifacts/c129d84767ca7fe64514b3789c623e1203355949"
+
+    cmake .. \
+        -DCMAKE_INSTALL_PREFIX="${JULIA_ROOT_PATH}/dev/DACE_jll/override" \
+        -DCMAKE_PREFIX_PATH="${JULIA_ROOT_PATH}${CXX_WRAP_PREFIX_PATH}" \
+        -DCMAKE_INSTALL_RPATH="${JULIA_ROOT_PATH}${CXX_WRAP_PREFIX_PATH}/lib:${JULIA_ROOT_PATH}/juliaup/julia-1.10.2+0.x64.linux.gnu/lib" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DWITH_PTHREAD=ON \
+        -DWITH_ALGEBRAICMATRIX=ON \
+        -DCMAKE_CXX_STANDARD=20 \
+        -DWITH_JULIA=ON \
+        -LA
+fi

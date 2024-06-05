b82d811b2fbf881c0cc3af01b9bb48c6a7e9fe47
Alberto Fossà <alberto.fossa18@gmail.com>
2024-06-05 08:54:29 -0500

    bump to Julia 1.10.4 and libcxxwrap 0.12.5

--- configure.sh
+++ configure.sh
@@ -22,12 +22,12 @@ if [ -z "$1" ]; then
 else
     echo "Building dace with Julia interface"
 
-    CXX_WRAP_PREFIX_PATH="/artifacts/c129d84767ca7fe64514b3789c623e1203355949"
+    CXX_WRAP_PREFIX_PATH="/artifacts/6d40e7ea7d9da1197bf07d3970257e8a9260fe32"
 
     cmake .. \
         -DCMAKE_INSTALL_PREFIX="${JULIA_ROOT_PATH}/dev/DACE_jll/override" \
         -DCMAKE_PREFIX_PATH="${JULIA_ROOT_PATH}${CXX_WRAP_PREFIX_PATH}" \
-        -DCMAKE_INSTALL_RPATH="${JULIA_ROOT_PATH}${CXX_WRAP_PREFIX_PATH}/lib:${JULIA_ROOT_PATH}/juliaup/julia-1.10.2+0.x64.linux.gnu/lib" \
+        -DCMAKE_INSTALL_RPATH="${JULIA_ROOT_PATH}${CXX_WRAP_PREFIX_PATH}/lib:${JULIA_ROOT_PATH}/juliaup/julia-1.10.4+0.x64.linux.gnu/lib" \
         -DCMAKE_BUILD_TYPE=Release \
         -DWITH_PTHREAD=ON \
         -DWITH_ALGEBRAICMATRIX=ON \
